4fa2251dc903644b4da15ba22089d0420cc3b676
dev-do <dev-do@naver.com>
2020-08-16 00:33:32 +0900

    firebase 설정과 npx create-react-app으로 프로젝트 생성

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCfZJwhBMNcg1rymPCISwoumONhYExp4W4",
+  authDomain: "clone-c2ce8.firebaseapp.com",
+  databaseURL: "https://clone-c2ce8.firebaseio.com",
+  projectId: "clone-c2ce8",
+  storageBucket: "clone-c2ce8.appspot.com",
+  messagingSenderId: "294378011439",
+  appId: "1:294378011439:web:c8e6fee8d5eea9c6667fda",
+  measurementId: "G-08C62G881D",
+};

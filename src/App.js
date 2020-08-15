b7f7ca62c85715b8d9c7a9c9d58721d323dd6388
dev-do <dev-do@naver.com>
2020-08-16 02:36:48 +0900

    header와 material icon 설치

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     // BEM class naming convention을 사용
     <div className="app">
-      <h1>Hello Clever Programmer, let's build a youtube clone</h1>
+      {/* <h1>Hello Clever Programmer, let's build a youtube clone</h1> */}
+
+      <Header />
+      {/* Header 헤더 -> <Header />*/}
+
+      {/* Sidebar 사이드바 -> <Sidebar /> */}
+      {/* RecommendedVideo 추천 동영상 */}
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// es7 snippets ( vscode extension 에서 다운로드 받을 것 )
+// rfce 를 치면 기본 값 생성
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>I am a header</h1>
+    </div>
+  );
+}
+
+export default Header;

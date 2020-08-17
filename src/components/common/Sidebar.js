f3cea539bce116a7fd589cfb867258bf96467c79
dev-do <dev-do@naver.com>
2020-08-17 18:45:08 +0900

    sidebar 추가적으로 작업 완료 후 recommended 작업 진행

--- src/components/common/Sidebar.js
+++ src/components/common/Sidebar.js
@@ -4,9 +4,23 @@ import SidebarRow from "./SidebarRow";
 import HomeIcon from "@material-ui/icons/Home";
 import WhatshotIcon from "@material-ui/icons/Whatshot";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMore";
 
 const SidebarWrap = styled.div`
   flex: 0.2;
+
+  hr {
+    height: 1px;
+    border: 0;
+    background-color: lightgray;
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
 `;
 
 function Sidebar() {
@@ -15,6 +29,14 @@ function Sidebar() {
       <SidebarRow selected title="Home" Icon={HomeIcon} />
       <SidebarRow title="Trending" Icon={WhatshotIcon} />
       <SidebarRow title="Subscription" Icon={SubscriptionsIcon} />
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+      <SidebarRow Icon={HistoryIcon} title="History" />
+      <SidebarRow Icon={OndemandVideoIcon} title="Your videos" />
+      <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="Liked videos" />
+      <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Show more" />
+      <hr />
     </SidebarWrap>
   );
 }
--- src/components/main/RecommendedVideos.js
+++ src/components/main/RecommendedVideos.js
@@ -1,15 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import { VideoCard } from "../common";
 
 const RecommendedVideosWrap = styled.div`
   flex: 0.8;
-  border: 1px solid black;
+  background-color: #f9f9f9;
+  padding: 40px 20px;
+  padding-bottom: 0;
+  .recommendedVideos {
+    h2 {
+      margin-left: 5px;
+      margin-bottom: 20px;
+    }
+  }
 `;
 
 function RecommendedVideos() {
   return (
     <RecommendedVideosWrap>
-      <h2>This is where videos go</h2>
+      <div className="recommendedVideos">
+        <h2>Recommended</h2>
+        <div className="recommendedVideos__videos">
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+          <VideoCard />
+        </div>
+      </div>
     </RecommendedVideosWrap>
   );
 }
